48ec7a1c74255c07d71bb254a6bf3341702f29c6
Sergio León <56183278+Sleon4@users.noreply.github.com>
2022-04-06 07:26:02 -0500

    Unnecessary controllers and models have been removed, unnecessary paths have been removed.

--- routes/web.php
+++ routes/web.php
@@ -5,31 +5,9 @@ use LionRoute\Route;
 use App\Http\Middleware\JWT\AuthorizationControl;
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Auth\{ LoginController, RegisterController, DocumentTypesController };
-use App\Http\Controllers\Users\ProfileController;
 
-Route::init([
-    'middleware' => [
-        Route::newMiddleware('exist-jwt', AuthorizationControl::class, 'exist'),
-        Route::newMiddleware('authorize-jwt', AuthorizationControl::class, 'authorize')
-    ]
-]);
+Route::init();
 
 Route::any('/', [HomeController::class, 'index']);
 
-Route::prefix('api', function() {
-    Route::get('read-document-types', [DocumentTypesController::class, 'readDocumentTypes']);
-
-    Route::prefix('auth', function() {
-        Route::post('signin', [LoginController::class, 'auth']);
-        Route::post('signup', [RegisterController::class, 'createUser']);
-    });
-
-    Route::middleware(['exist-jwt', 'authorize-jwt'], function() {
-        Route::post('example-athorize', [ProfileController::class, 'info']);
-    });
-});
-
-Route::processOutput(
-    Route::dispatch(3)
-);
+Route::processOutput(Route::dispatch(3));
