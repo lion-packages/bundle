3a8470dcc00b88ad4b704cc90346a7e6c80416f7
Sergio León <sergioleon4004@hotmail.com>
2022-05-27 11:57:28 -0500

    Build: Composer dependencies have been updated, new namespaces have been imported

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use App\Http\Request\Request;
-use LionSql\Drivers\MySQLDriver as Builder;
+use LionSQL\Drivers\MySQLDriver as Builder;
 
 class Model {
 
@@ -18,6 +18,7 @@ class Model {
 
 		Builder::init([
 			'host' => isset($config['host']) ? $config['host'] : $this->env->DB_HOST,
+            'port' => isset($config['port']) ? $config['port'] : $this->env->DB_PORT,
 			'db_name' => isset($config['db_name']) ? $config['db_name'] : $this->env->DB_NAME,
 			'user' => isset($config['user']) ? $config['user'] : $this->env->DB_USER,
 			'password' => isset($config['password']) ? $config['password'] : $this->env->DB_PASSWORD,
