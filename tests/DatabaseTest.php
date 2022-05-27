0497f2e1d46ce420c396d75ab00fbd44363edf8f
Sergio León <sergioleon4004@hotmail.com>
2022-05-27 12:23:07 -0500

    Test(DatabaseTest): New test to check the established connection to the database

---
+++ tests/DatabaseTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use LionSQL\Drivers\MySQLDriver as Builder;
+
+class DatabaseTest extends TestCase {
+
+    public function setUp(): void {
+        (\Dotenv\Dotenv::createImmutable(__DIR__))->load();
+    }
+
+    public function testConnection(): void {
+        $responseConnect = Builder::init([
+            'host' => $_ENV['DB_HOST'],
+            'port' => $_ENV['DB_PORT'],
+            'db_name' => $_ENV['DB_NAME'],
+            'user' => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASSWORD'],
+            'charset' => $_ENV['DB_CHARSET']
+        ]);
+
+        $this->assertEquals('success', $responseConnect->status);
+    }
+
+}
