06e24d7f30eb64c21b9975cc78f8ccfcd35e0672
Sergio León <sergioleon4004@hotmail.com>
2022-06-02 14:29:26 -0500

    Style(TestCommand): Styles have been added in command display

--- app/Console/TestCommand.php
+++ app/Console/TestCommand.php
@@ -11,19 +11,19 @@ class TestCommand extends Command {
 	protected static $defaultName = "test";
 
 	protected function initialize(InputInterface $input, OutputInterface $output) {
+        $output->writeln("<comment>Running unit tests...</comment>");
+    }
 
-	}
+    protected function interact(InputInterface $input, OutputInterface $output) {
 
-	protected function interact(InputInterface $input, OutputInterface $output) {
+    }
 
-	}
+    protected function configure() {
+        $this->setDescription("Command to create run unit tests");
+    }
 
-	protected function configure() { 
-		$this->setDescription("Command to create run unit tests");
-	}
-
-	protected function execute(InputInterface $input, OutputInterface $output) {
-		$result = exec('.\vendor\bin\phpunit');
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $result = exec('.\vendor\bin\phpunit');
         $output->writeln($result);
         return Command::SUCCESS;
     }
