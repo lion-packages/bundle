bddb85dca602ad4f1022e4c05e5b6acd5b5cc0b0
Sergio León <sergioleon4004@hotmail.com>
2022-06-07 19:32:43 -0500

    Feat: Integrated server initialization from 'public' directory

--- public/index.php
+++ public/index.php
@@ -7,17 +7,16 @@
 * @author   Sergio León <sergioleon4004@hotmail.com>
 **/
 
-require_once("vendor/autoload.php");
-(Dotenv\Dotenv::createImmutable(__DIR__))->load();
+require_once(__DIR__ . "/../vendor/autoload.php");
+(Dotenv\Dotenv::createImmutable(__DIR__ . "/../"))->load();
 
 header("Access-Control-Allow-Origin: {$_ENV['SERVER_ACCESS_CONTROL_ALLOW_ORIGIN']}");
 header("Access-Control-Max-Age: {$_ENV['SERVER_ACCESS_CONTROL_MAX_AGE']}");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Allow: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Content-Type: application/json; charset=UTF-8");
 date_default_timezone_set($_ENV['SERVER_DEFAULT_TIME_ZONE']);
 
 LionRoute\Route::init();
-include_once("routes/web.php");
+include_once("../routes/web.php");
 LionRoute\Route::dispatch();
