e17b7e2da022175f6309b0158b7a0c2c38f26e77
Sergio León <sergioleon4004@hotmail.com>
2022-06-18 09:23:41 -0500

    Feat: added new 'header' file to modify header

--- public/index.php
+++ public/index.php
@@ -9,13 +9,7 @@
 
 require_once(__DIR__ . "/../vendor/autoload.php");
 (Dotenv\Dotenv::createImmutable(__DIR__ . "/../"))->load();
-
-header("Access-Control-Allow-Origin: {$_ENV['SERVER_ACCESS_CONTROL_ALLOW_ORIGIN']}");
-header("Access-Control-Max-Age: {$_ENV['SERVER_ACCESS_CONTROL_MAX_AGE']}");
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Content-Type: application/json; charset=UTF-8");
-date_default_timezone_set($_ENV['SERVER_DEFAULT_TIME_ZONE']);
+include_once("../routes/header.php");
 
 LionRoute\Route::init();
 include_once("../routes/middleware.php");
---
+++ routes/header.php
@@ -0,0 +1,9 @@
+<?php
+
+header("Access-Control-Allow-Origin: {$_ENV['SERVER_ACCESS_CONTROL_ALLOW_ORIGIN']}");
+header("Access-Control-Max-Age: {$_ENV['SERVER_ACCESS_CONTROL_MAX_AGE']}");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Content-Type: application/json; charset=UTF-8");
+
+date_default_timezone_set($_ENV['SERVER_DEFAULT_TIME_ZONE']);
