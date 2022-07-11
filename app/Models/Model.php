67cc2c22a0d098cfa60eccc8a776732b400fdabc
Sergio León <sergioleon4004@hotmail.com>
2022-07-11 12:06:44 -0500

    Build: updated .env.example

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use LionRequest\Response;
+
 class Model {
 
 	protected object $env;
@@ -10,17 +12,26 @@ class Model {
 
 	}
 
-	protected function init(array $config = []): void {
-		$this->env = \LionRequest\Request::getInstance()->env();
+    private function processOutput($response): void {
+        echo(json_encode($response));
+        exit();
+    }
 
-		\LionSQL\Drivers\MySQLDriver::init([
-			'host' => isset($config['host']) ? $config['host'] : $this->env->DB_HOST,
+    protected function init(array $config = []): void {
+        $this->env = \LionRequest\Request::getInstance()->env();
+
+        $response_conn = \LionSQL\Drivers\MySQLDriver::init([
+            'host' => isset($config['host']) ? $config['host'] : $this->env->DB_HOST,
             'port' => isset($config['port']) ? $config['port'] : $this->env->DB_PORT,
-			'db_name' => isset($config['db_name']) ? $config['db_name'] : $this->env->DB_NAME,
-			'user' => isset($config['user']) ? $config['user'] : $this->env->DB_USER,
-			'password' => isset($config['password']) ? $config['password'] : $this->env->DB_PASSWORD,
-			'charset' => isset($config['charset']) ? $config['charset'] : $this->env->DB_CHARSET
-		]);
-	}
+            'db_name' => isset($config['db_name']) ? $config['db_name'] : $this->env->DB_NAME,
+            'user' => isset($config['user']) ? $config['user'] : $this->env->DB_USER,
+            'password' => isset($config['password']) ? $config['password'] : $this->env->DB_PASSWORD,
+            'charset' => isset($config['charset']) ? $config['charset'] : $this->env->DB_CHARSET
+        ]);
+
+        if ($response_conn->status === 'error') {
+            $this->processOutput(Response::error($response_conn->message));
+        }
+    }
 
 }
