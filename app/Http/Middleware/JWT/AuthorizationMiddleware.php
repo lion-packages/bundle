e21fdbf06a3a6baa0831a60de63d6dc9e8af45dc
Sergio León <sergioleon4004@hotmail.com>
2022-07-29 20:37:48 -0500

    Feat: added middleware for JWT authorization and authentication

---
+++ app/Http/Middleware/JWT/AuthorizationMiddleware.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Middleware\JWT;
+
+use LionSecurity\JWT;
+
+class AuthorizationMiddleware {
+
+    public function __construct() {
+
+    }
+
+    public function exist(): void {
+        $headers = apache_request_headers();
+
+        if (!isset($headers['Authorization'])) {
+            response->finish(
+                json->encode(
+                    response->error('The JWT does not exist')
+                )
+            );
+        }
+    }
+
+    public function authorize(): void {
+        $headers = apache_request_headers();
+
+        if (preg_match('/Bearer\s(\S+)/', $headers['Authorization'], $matches)) {
+            $jwt = JWT::decode($matches[1]);
+
+            if ($jwt->status === 'error') {
+                response->finish(
+                    json->encode(
+                        response->error($jwt)
+                    )
+                );
+            }
+        } else {
+            response->finish(
+                json->encode(
+                    response->error('Invalid JWT')
+                )
+            );
+        }
+    }
+
+    public function notAuthorize(): void {
+        $headers = apache_request_headers();
+
+        if (isset($headers['Authorization'])) {
+            response->finish(
+                json->encode(
+                    response->error('User in session, You must close the session')
+                )
+            );
+        }
+    }
+
+}
