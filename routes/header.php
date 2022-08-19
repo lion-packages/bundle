1325a2f69b01fa90749e3e27c288a2c851033b41
Sergio León <sergioleon4004@hotmail.com>
2022-08-18 20:28:04 -0500

    Build: updated composer

--- routes/header.php
+++ routes/header.php
@@ -10,27 +10,8 @@
 
 date_default_timezone_set($_ENV['SERVER_DEFAULT_TIME_ZONE']);
 
-LionRequest\Request::header(
-    'Content-Type',
-    'application/json; charset=UTF-8'
-);
-
-LionRequest\Request::header(
-    'Access-Control-Allow-Origin',
-    env->SERVER_ACCESS_CONTROL_ALLOW_ORIGIN
-);
-
-LionRequest\Request::header(
-    'Access-Control-Max-Age',
-    env->SERVER_ACCESS_CONTROL_MAX_AGE
-);
-
-LionRequest\Request::header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE'
-);
-
-LionRequest\Request::header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-);
+LionRequest\Request::header('Content-Type', 'application/json; charset=UTF-8');
+LionRequest\Request::header('Access-Control-Allow-Origin', env->SERVER_ACCESS_CONTROL_ALLOW_ORIGIN);
+LionRequest\Request::header('Access-Control-Max-Age', env->SERVER_ACCESS_CONTROL_MAX_AGE);
+LionRequest\Request::header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+LionRequest\Request::header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
