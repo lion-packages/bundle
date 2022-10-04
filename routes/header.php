b055b146b742032b04728980710e3aa9d9b2c162
Sergio León <sergioleon4004@hotmail.com>
2022-10-03 22:37:02 -0500

    Fix: corrected variables

--- routes/header.php
+++ routes/header.php
@@ -8,10 +8,10 @@
  * ------------------------------------------------------------------------------
  **/
 
-date_default_timezone_set($_ENV['SERVER_DEFAULT_TIME_ZONE']);
+date_default_timezone_set("America/Bogota");
 
 LionRequest\Request::header('Content-Type', 'application/json; charset=UTF-8');
-LionRequest\Request::header('Access-Control-Allow-Origin', env->SERVER_ACCESS_CONTROL_ALLOW_ORIGIN);
-LionRequest\Request::header('Access-Control-Max-Age', env->SERVER_ACCESS_CONTROL_MAX_AGE);
+LionRequest\Request::header('Access-Control-Allow-Origin', "*");
+LionRequest\Request::header('Access-Control-Max-Age', "3600");
 LionRequest\Request::header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 LionRequest\Request::header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
