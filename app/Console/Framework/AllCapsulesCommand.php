cdbbc8254c063fa320cfd59496778b561f58db6b
Sergio León <sergioleon4004@hotmail.com>
2022-10-10 09:36:40 -0500

    Feat: added command

---
+++ app/Console/Framework/AllCapsulesCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Framework;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{ InputInterface, InputArgument, InputOption, ArrayInput };
+use Symfony\Component\Console\Output\OutputInterface;
+use LionCommand\Functions\{ FILES, ClassPath };
+use LionSQL\Drivers\MySQLDriver as Builder;
+
+class AllCapsulesCommand extends Command {
+
+    protected static $defaultName = "new:all-capsule";
+    private string $default_path = "app/Class/";
+
+    protected function initialize(InputInterface $input, OutputInterface $output) {
+        $output->writeln("<comment>Creating all the capsules...</comment>");
+
+        Builder::init([
+            'host' => env->DB_HOST,
+            'port' => env->DB_PORT,
+            'db_name' => env->DB_NAME,
+            'user' => env->DB_USER,
+            'password' => env->DB_PASSWORD
+        ]);
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output) {
+
+    }
+
+    protected function configure() {
+        $this->setDescription(
+            'Command required for the creation of all new Capsules available from the database'
+        )->addArgument(
+            'capsule', InputArgument::REQUIRED, '', null
+        )->addOption(
+            'path', null, InputOption::VALUE_REQUIRED, 'Do you want to configure your own route?'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $database = $input->getArgument('capsule');
+        $path = $input->getOption('path');
+        $all_tables = Builder::showTables(trim($database));
+
+        foreach ($all_tables as $keyTables => $tableDB) {
+            $table = $tableDB->{"Tables_in_" . env->DB_NAME};
+
+            $this->getApplication()->find('new:capsule')->run(
+                new ArrayInput([
+                    'capsule' => $table,
+                    '--path' => ($path === null ? false : $path)
+                ]),
+                $output
+            );
+        }
+
+        return Command::SUCCESS;
+    }
+
+}
