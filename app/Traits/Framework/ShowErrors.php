c24dd99782ec79cb784ec0619bbfe8577618a4f7
Sergio León <sergioleon4004@hotmail.com>
2022-11-24 21:47:57 -0500

    CI: files have been added

---
+++ app/Traits/Framework/ShowErrors.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Traits\Framework;
+
+use \Closure;
+use LionSecurity\Validation;
+
+trait ShowErrors {
+
+    private static array $validation;
+
+    public static function validate(Closure $validate_function): void {
+        $response = Validation::validate((array) request, $validate_function);
+        self::$validation = $response->status === 'error' ? $response->messages : [];
+    }
+
+    public static function display(): void {
+        if (count(self::$validation) > 0) {
+            foreach (self::$validation as $keyErrors => $errors) {
+                response->finish(response->error($errors[0]));
+            }
+        }
+    }
+
+}
