9d201f5707ffa8f8b5804581dc59f233239ebcd1
Sergio León <sergioleon4004@hotmail.com>
2023-01-26 13:55:47 -0500

    fix: function call bug fixed

--- app/Models/Auth/LoginModel.php
+++ app/Models/Auth/LoginModel.php
@@ -7,11 +7,15 @@ use LionSQL\Drivers\MySQL as DB;
 class LoginModel {
 
 	public function __construct() {
-		return DB::table('users')
+
+	}
+
+    public function authDB() {
+        return DB::table('users')
             ->select(DB::alias(DB::count('*'), "cont"))
             ->where(DB::equalTo("users_email"), request->users_email)
             ->and(DB::equalTo("users_password"), request->users_password)
             ->get();
-	}
+    }
 
 }
