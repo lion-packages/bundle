d49046924014f42a8719100a33c4a4715c19b397
Sergio León <sergioleon4004@hotmail.com>
2023-03-08 14:03:51 -0500

    build: composer updated

--- app/Models/Auth/LoginModel.php
+++ app/Models/Auth/LoginModel.php
@@ -14,7 +14,6 @@ class LoginModel {
         return DB::table('users')
             ->select(DB::as(DB::count('*'), "cont"))
             ->where(DB::equalTo("users_email"), request->users_email)
-            ->and(DB::equalTo("users_password"), request->users_password)
             ->get();
     }
 
