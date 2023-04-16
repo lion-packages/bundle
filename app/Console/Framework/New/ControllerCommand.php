a9ac7c4e51390bc80cc22acc54f76a3ead624ecf
Sergio León <sergioleon4004@hotmail.com>
2023-04-16 12:37:34 -0500

    feat: added option to create models when creating controllers

--- app/Console/Framework/New/ControllerCommand.php
+++ app/Console/Framework/New/ControllerCommand.php
@@ -3,7 +3,7 @@
 namespace App\Console\Framework\New;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\{ InputInterface, InputArgument };
+use Symfony\Component\Console\Input\{ArrayInput, InputInterface, InputArgument, InputOption};
 use Symfony\Component\Console\Output\OutputInterface;
 use LionFiles\Store;
 use App\Traits\Framework\ClassPath;
@@ -25,10 +25,13 @@ class ControllerCommand extends Command {
             'Command required for the creation of new Controllers'
         )->addArgument(
             'controller', InputArgument::REQUIRED, '', null
+        )->addOption(
+            'model', null, InputOption::VALUE_REQUIRED, 'Do you want to create the model?'
         );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
+        $model = $input->getOption('model');
         $list = ClassPath::export("app/Http/Controllers/", $input->getArgument('controller'));
         $url_folder = lcfirst(str_replace("\\", "/", $list['namespace']));
         Store::folder($url_folder);
@@ -47,6 +50,14 @@ class ControllerCommand extends Command {
         ClassPath::close();
 
         $output->writeln("<info>Controller created successfully</info>");
+
+        if ($model != null) {
+            $this->getApplication()->find('new:model')->run(
+                new ArrayInput(['model' => $model]),
+                $output
+            );
+        }
+
         return Command::SUCCESS;
     }
 
