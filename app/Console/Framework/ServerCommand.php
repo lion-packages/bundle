40fcb1b7eafd2e82df9a82eec984d40abf5d80fb
Sergio León <sergioleon4004@hotmail.com>
2023-04-30 15:35:06 -0500

    feat: command to modify host when starting local server has been modified

--- app/Console/Framework/ServerCommand.php
+++ app/Console/Framework/ServerCommand.php
@@ -24,20 +24,27 @@ class ServerCommand extends Command {
             "created command to start server locally"
         )->addOption(
             'port', null, InputOption::VALUE_REQUIRED, 'Do you want to set your own port?'
+        )->addOption(
+            'host', null, InputOption::VALUE_REQUIRED, 'Do you want to set your own host?'
         );
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$port = $input->getOption('port');
+        $host = $input->getOption('host');
 
         if ($port === null) {
             $port = 8000;
         }
 
-        $host = "127.0.0.1:{$port}";
-        $output->writeln("\t<question> INFO </question> Server running on <href=http://{$host}>[http://{$host}]</>\n");
+        if ($host === null) {
+            $host = "127.0.0.1";
+        }
+
+        $url = "{$host}:{$port}";
+        $output->writeln("\t<question> INFO </question> Server running on <href=http://{$url}>[http://{$url}]</>\n");
         $output->writeln("<comment>Press Ctrl+C to stop the server</comment>\n");
-        exec("php -S $host -t public");
+        exec("php -S {$host}:{$port} -t public");
 
         return Command::SUCCESS;
 	}
