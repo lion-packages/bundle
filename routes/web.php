d894bda48092b2843b4783069f7728e3c1ada40f
Sergio León <sergioleon4004@hotmail.com>
2023-05-06 09:17:46 -0500

    feat: session controller has been created to refresh JWT token

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Auth\JWTController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\SessionController;
 use LionRoute\Route;
 
 /**
@@ -16,7 +16,7 @@ Route::get('/', fn() => info("Welcome to index"));
 
 Route::prefix('api', function() {
     Route::prefix('auth', function() {
-        Route::post('login', [LoginController::class, 'auth']);
-        Route::post('refresh', [JWTController::class, 'refresh'], ['jwt-authorize']);
+        Route::post('login', [LoginController::class, 'auth'], ['jwt-not-authorize']);
+        Route::get('refresh', [SessionController::class, 'refresh'], ['jwt-authorize']);
     });
 });
