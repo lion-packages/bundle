f3a89919d471fe4a4b9c32b8b484e7f9c61bb695
Sergio León <sergioleon4004@hotmail.com>
2023-05-10 11:17:57 -0500

    style: spaces have been removed

--- config/database.php
+++ config/database.php
@@ -18,22 +18,6 @@ return [
             'dbname' => env->DB_NAME,
             'user' => env->DB_USER,
             'password' => env->DB_PASSWORD
-        ],
-        env->DB_NAME_CONTABILIDAD => [
-            'type' => env->DB_TYPE_CONTABILIDAD,
-            'host' => env->DB_HOST_CONTABILIDAD,
-            'port' => env->DB_PORT_CONTABILIDAD,
-            'dbname' => env->DB_NAME_CONTABILIDAD,
-            'user' => env->DB_USER_CONTABILIDAD,
-            'password' => env->DB_PASSWORD_CONTABILIDAD
-        ],
-        env->DB_NAME_DIGITALIZACION => [
-            'type' => env->DB_TYPE_DIGITALIZACION,
-            'host' => env->DB_HOST_DIGITALIZACION,
-            'port' => env->DB_PORT_DIGITALIZACION,
-            'dbname' => env->DB_NAME_DIGITALIZACION,
-            'user' => env->DB_USER_DIGITALIZACION,
-            'password' => env->DB_PASSWORD_DIGITALIZACION
         ]
     ]
 ];
