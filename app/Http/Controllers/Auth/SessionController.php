4b47960ff3eb3bb46eda7d7af841d676817fd3c8
Sergio León <sergioleon4004@hotmail.com>
2023-05-15 16:56:41 -0500

    ci: function has been validated to refresh jwt token

--- app/Http/Controllers/Auth/SessionController.php
+++ app/Http/Controllers/Auth/SessionController.php
@@ -11,10 +11,17 @@ class SessionController {
     }
 
     public function refresh() {
+        $jwt = JWT::decode(JWT::get())->data;
+
+        if (isset($jwt->refresh) && $jwt->refresh >= 3) {
+            return error("you have exceeded the maximum amount of session refresh");
+        }
+
         return success("refreshed session token", [
-            'jwt' => JWT::encode(
-                (array) JWT::decode(JWT::get())->data
-            )
+            'jwt' => JWT::encode([
+                'refresh' => 1,
+                ...((array) $jwt)
+            ])
         ]);
     }
 
