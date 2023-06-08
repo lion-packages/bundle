c8380f87715cbbacf1ff16a74900e85a6a901758
Sergio León <sergioleon4004@hotmail.com>
2023-06-08 11:26:59 -0500

    refactor: command to generate keys with RSA has been renamed

---
+++ app/Console/Framework/RSA/NewRSACommand.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Framework\RSA;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{ InputInterface, InputOption };
+use Symfony\Component\Console\Output\OutputInterface;
+use LionSecurity\RSA;
+use LionFiles\Store;
+
+class NewRSACommand extends Command {
+
+	protected static $defaultName = "rsa:new";
+
+	protected function initialize(InputInterface $input, OutputInterface $output) {
+		$output->writeln("<comment>Initializing RSA service...</comment>");
+	}
+
+	protected function interact(InputInterface $input, OutputInterface $output) {
+
+	}
+
+	protected function configure() {
+		$this
+            ->setDescription("Command to create public and private keys with RSA")
+            ->addOption('path', 'p', InputOption::VALUE_REQUIRED, 'Save to a specific path?');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+        $path = $input->getOption('path');
+
+        RSA::$url_path = $path === null ? RSA::$url_path : storage_path($path, false);
+		Store::folder(RSA::$url_path);
+		RSA::createKeys();
+		Store::remove('.rnd');
+		$output->writeln("<info>Public and private key created successfully</info>");
+
+		return Command::SUCCESS;
+	}
+
+}
