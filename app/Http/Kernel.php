c46e2f77555931a9adf395b4e24539cfe87cb391
Sergio Leon <sergioleon4004@hotmail.com>
2023-07-10 09:27:26 -0500

    feat: trait session has been added to modify and create sessions from the Kernel (http)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,30 +3,11 @@
 namespace App\Http;
 
 use App\Traits\Framework\HttpTrait;
+use App\Traits\Framework\Session;
 use App\Traits\Framework\Singleton;
 
 class Kernel {
 
-    use Singleton, HttpTrait;
-
-    public function new(string $name, mixed $value = null): Kernel {
-        $_SESSION[$name] = $value;
-        return $this;
-    }
-
-    public function get(mixed $key = null): mixed {
-        return $key === null ? $_SESSION : $_SESSION[$key];
-    }
-
-    public function destroy(mixed $key = null): Kernel {
-        if ($key === null) {
-            session_unset();
-            session_destroy();
-        } else {
-            unset($_SESSION[$key]);
-        }
-
-        return $this;
-    }
+    use Singleton, HttpTrait, Session;
 
 }
---
+++ app/Traits/Framework/Session.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Traits\Framework;
+
+trait Session {
+
+    public function new(string $name, mixed $value = null): static {
+        $_SESSION[$name] = $value;
+        return $this;
+    }
+
+    public function get(mixed $key = null): mixed {
+        return $key === null ? $_SESSION : $_SESSION[$key];
+    }
+
+    public function destroy(mixed $key = null): static {
+        if ($key === null) {
+            session_unset();
+            session_destroy();
+        } else {
+            unset($_SESSION[$key]);
+        }
+
+        return $this;
+    }
+
+}
