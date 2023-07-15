b6d4eb0888209c2f634885e567bf23e0b7826d38
Sergio Leon <sergioleon4004@hotmail.com>
2023-07-15 14:05:26 -0500

    ci: command to export databases has been modified

--- app/Console/Framework/DB/ExportDatabaseCommand.php
+++ app/Console/Framework/DB/ExportDatabaseCommand.php
@@ -5,11 +5,9 @@ namespace App\Console\Framework\DB;
 use App\Traits\Framework\ConsoleOutput;
 use Carbon\Carbon;
 use LionDatabase\Drivers\MySQL\MySQL as DB;
-use LionFiles\Store;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ExportDatabaseCommand extends Command {
@@ -40,7 +38,7 @@ class ExportDatabaseCommand extends Command {
         if ($info['type'] === "mysql") {
             $path = __DIR__ . "/../../../../storage/backups/database/";
             $file_name = "{$info['dbname']}_{$actual_date}.sql";
-            kernel->execute("MYSQL_PWD={$info['password']} mysqldump -h {$info['host']} -u {$info['user']} -p --routines --triggers --events {$info['dbname']} > {$path}{$file_name}");
+            kernel->execute("mysqldump -h {$info['host']} --user='{$info['user']}' --password='{$info['password']}' --routines --triggers --events --add-drop-table --dump-date --hex-blob --order-by-primary --single-transaction --disable-keys --add-drop-database {$info['dbname']} > {$path}{$file_name}");
             $output->writeln($this->successOutput("DATABASE: {$info['dbname']}"));
             $output->writeln($this->warningOutput("DATABASE: exported database in ./storage/backups/database/{$info['dbname']}_{$actual_date}.sql"));
         }
