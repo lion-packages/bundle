d222e519231e357c8809f541598e2b1d6f98acb5
Sergio Leon <sergioleon4004@hotmail.com>
2023-07-17 22:10:41 -0500

    ci: rules have been added

---
+++ app/Rules/LionDatabase/Users/UsersNameRule.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Rules\LionDatabase\Users;
+
+use App\Traits\Framework\ShowErrors;
+
+class UsersNameRule {
+
+	use ShowErrors;
+
+	public static string $field = "users_name";
+	public static string $desc = "";
+	public static string $value = "";
+	public static bool $disabled = false;
+
+	public static function passes(): void {
+		self::validate(function(\Valitron\Validator $validator) {
+			$validator->rule("required", self::$field)->message("property is required");
+		});
+	}
+
+}
