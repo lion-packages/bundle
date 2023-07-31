be592654bf938052fd6b1b01c0702741f8e00665
Sergio Leon <sergioleon4004@hotmail.com>
2023-07-31 13:22:55 -0500

    ci: resource settings updated

--- config/resources.php
+++ config/resources.php
@@ -17,29 +17,11 @@ return [
             'type' => 'vite',
             'path' => 'lion-dev/',
         ],
-        'console-web' => [
-            'type' => 'twig',
-            'host' => '0.0.0.0',
-            'port' => 7000,
-            'path' => 'console-web/',
-        ],
         'available-urls' => [
             'type' => 'twig',
             'host' => '0.0.0.0',
-            'port' => 7001,
+            'port' => 7000,
             'path' => 'available-urls/',
-        ],
-        'login' => [
-            'type' => 'twig',
-            'host' => '0.0.0.0',
-            'port' => 7002,
-            'path' => 'login/',
-        ],
-        'user-registration' => [
-            'type' => 'twig',
-            'host' => '0.0.0.0',
-            'port' => 7003,
-            'path' => 'user-registration/',
         ],
     ],
 ];
