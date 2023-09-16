70ca74b16af14a0c93ad17c28777ba33cb617c7c
Sergio Leon <sergio.leon@gopenux.com>
2023-09-16 12:57:05 -0500

    feat: options have been added to the test command to run specific tests

--- app/Console/Framework/RunTestCommand.php
+++ app/Console/Framework/RunTestCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Framework;
 use App\Traits\Framework\ConsoleOutput;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class RunTestCommand extends Command
@@ -26,12 +27,29 @@ class RunTestCommand extends Command
 	protected function configure()
 	{
 		$this
-            ->setDescription("Command to create run unit tests");
+            ->setDescription("Command to create run unit tests")
+            ->addOption('class', 'c', InputOption::VALUE_OPTIONAL, 'Do you want to run a specific class?', false)
+            ->addOption('method', 'm', InputOption::VALUE_OPTIONAL, 'Do you want to filter a specific method?', false);
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
-		$result = exec('./vendor/bin/phpunit');
+		$result = '';
+		$class = $input->getOption('class');
+		$method = $input->getOption('method');
+
+		if (!$class && !$method) {
+			$result = exec('./vendor/bin/phpunit');
+		}
+
+		if ($class && !$method) {
+			$result = exec("./vendor/bin/phpunit tests/{$class}.php");
+		}
+
+		if ($class && $method) {
+			$result = exec("./vendor/bin/phpunit tests/{$class}.php --filter {$method}");
+		}
+
         $output->writeln($this->warningOutput("\t>>  {$result}"));
         return Command::SUCCESS;
 	}
