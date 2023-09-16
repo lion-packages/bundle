058b35436337d1713cd5aef2d4c7d0f9da872234
Sergio Leon <sergio.leon@gopenux.com>
2023-09-16 14:55:27 -0500

    feat: PSR standards have been applied for creating files in the new:command command

--- app/Console/Framework/New/CommandsCommand.php
+++ app/Console/Framework/New/CommandsCommand.php
@@ -41,7 +41,7 @@ class CommandsCommand extends Command
         Store::folder($url_folder);
 
         $this->create($url_folder, $list['class']);
-        $this->add("<?php\n\n");
+        $this->add("<?php\n\ndeclare(strict_types=1);\n\n");
         $this->add("namespace {$list['namespace']};\n\n");
         $this->add("use App\Traits\Framework\ConsoleOutput;\n");
         $this->add("use Symfony\Component\Console\Command\Command;\n");
@@ -49,20 +49,20 @@ class CommandsCommand extends Command
         $this->add("use Symfony\Component\Console\Input\InputInterface;\n");
         $this->add("use Symfony\Component\Console\Input\InputOption;\n");
         $this->add("use Symfony\Component\Console\Output\OutputInterface;\n\n");
-        $this->add("class {$list['class']} extends Command {\n\n");
+        $this->add("class {$list['class']} extends Command\n{\n");
         $this->add("\t" . 'use ConsoleOutput;' . "\n\n");
         $this->add("\t" . 'protected static $defaultName = "";' . "\n\n");
-        $this->add("\t" . 'protected function initialize(InputInterface $input, OutputInterface $output) {' . "\n\n\t}\n\n");
-        $this->add("\t" . 'protected function interact(InputInterface $input, OutputInterface $output) {' . "\n\n\t}\n\n");
-        $this->add("\t" . "protected function configure() {\n\t\t" . '$this->setDescription("");' . "\n\t}\n\n");
-        $this->add("\t" . 'protected function execute(InputInterface $input, OutputInterface $output) {' . "\n");
+        $this->add("\t" . 'protected function initialize(InputInterface $input, OutputInterface $output)' . "\n\t" . '{' . "\n\n\t}\n\n");
+        $this->add("\t" . 'protected function interact(InputInterface $input, OutputInterface $output)' . "\n\t" . '{'. "\n\n\t}\n\n");
+        $this->add("\t" . "protected function configure()\n\t{\n\t\t" . '$this->setDescription("");' . "\n\t}\n\n");
+        $this->add("\t" . 'protected function execute(InputInterface $input, OutputInterface $output)' . "\n\t" . '{'. "\n");
         $this->add("\t\t" . '$output->writeln("");' . "\n\t\t" . 'return Command::SUCCESS;' . "\n");
-        $this->add("\t}\n\n}");
+        $this->add("\t}\n}");
         $this->force();
         $this->close();
 
         $output->writeln($this->warningOutput("\t>>  COMMAND: {$command}"));
-        $output->writeln($this->successOutput("\t>>  COMMAND: The '{$list['namespace']}\\{$list['class']}' command has been generated"));
+        $output->writeln($this->successOutput("\t>>  COMMAND: the '{$list['namespace']}\\{$list['class']}' command has been generated"));
 
         return Command::SUCCESS;
 	}
