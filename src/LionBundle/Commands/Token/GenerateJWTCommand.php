3742fd37ada8dce5f706e844ead318e8fd652cf2
Sleon4 <sergioleon4004@hotmail.com>
2023-10-28 12:33:15 -0500

    build: Implemented lion/command command initializer

--- src/LionBundle/Commands/Token/GenerateJWTCommand.php
+++ src/LionBundle/Commands/Token/GenerateJWTCommand.php
@@ -1,50 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LionBundle\Commands\Token;
 
-use App\Traits\Framework\ConsoleOutput;
+use LionCommand\Command;
 use LionSecurity\JWT;
 use LionSecurity\RSA;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class GenerateJWTCommand extends Command
 {
-    use ConsoleOutput;
-
-    protected static $defaultName = "token:jwt";
-
-    protected function initialize(InputInterface $input, OutputInterface $output)
-    {
-
-    }
-
-    protected function interact(InputInterface $input, OutputInterface $output)
-    {
-
-    }
-
     protected function configure()
     {
         $this
-            ->setDescription("Created command to generate JWT token")
-            ->addArgument('session', InputArgument::OPTIONAL, 'Session must be true or false', "true")
-            ->addOption('path', 'p', InputOption::VALUE_REQUIRED, 'Save to a specific path?');
+            ->setName('token:jwt')
+            ->setDescription('Created command to generate JWT token')
+            ->addOption('path', 'p', InputOption::VALUE_REQUIRED, 'Save to a specific path?')
+            ->addOption('session', 's', InputOption::VALUE_OPTIONAL, 'Session must be true or false', true);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $path = $input->getOption("path");
-        if ($path != null) RSA::setPath($path);
+        RSA::setPath($path != null ? $path : RSA::getPath());
+
+        $session = $input->getOption('session');
+        $session = true === $session ? true : $session;
+        $session = 'true' === $session ? true : false;
 
         $jwt = JWT::encode([
-            'session' => $input->getArgument('session') === "true" ? true : false,
-            'system' => "Lion-Framework",
-            'autor' => "Sergio Leon",
-            'github' => "https://github.com/lion-packages"
+            'session' => $session,
+            'system' => 'Lion-Framework',
+            'autor' => 'Sergio Leon',
+            'github' => 'https://github.com/lion-packages'
         ]);
 
         $output->writeln($this->warningOutput("\t>>  TOKEN: JWT created successfully"));
