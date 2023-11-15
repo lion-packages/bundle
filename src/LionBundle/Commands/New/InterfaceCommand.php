eaf4a224a179c360199481d82d91695dbbc7a41b
Sleon4 <sergioleon4004@hotmail.com>
2023-11-15 14:51:07 -0500

    feat: InterfaceCommand command added

--- src/LionBundle/Commands/New/InterfaceCommand.php
+++ src/LionBundle/Commands/New/InterfaceCommand.php
@@ -1,66 +1,66 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LionBundle\Commands\New;
 
-use App\Traits\Framework\ClassPath;
-use App\Traits\Framework\ConsoleOutput;
+use LionBundle\Helpers\Commands\ClassFactory;
+use LionCommand\Command;
 use LionFiles\Store;
-use Symfony\Component\Console\Command\Command;
+use LionHelpers\Str;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class InterfaceCommand extends Command
 {
-    use ClassPath, ConsoleOutput;
+    private ClassFactory $classFactory;
+    private Store $store;
 
-	protected static $defaultName = "new:interface";
-
-	protected function initialize(InputInterface $input, OutputInterface $output)
+	protected function initialize(InputInterface $input, OutputInterface $output): void
 	{
-
+        $this->classFactory = new ClassFactory();
+        $this->store = new Store();
 	}
 
-	protected function interact(InputInterface $input, OutputInterface $output)
-	{
-
-	}
-
-	protected function configure()
+	protected function configure(): void
 	{
 		$this
-            ->setDescription("Command required for interface creation")
-            ->addArgument('interface', InputArgument::OPTIONAL, 'Interface name', "ExampleInterface");
+            ->setName('new:interface')
+            ->setDescription('Command required for interface creation')
+            ->addArgument('interface', InputArgument::OPTIONAL, 'Interface name', 'ExampleInterface');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		$interface = $input->getArgument('interface');
-        $list = $this->export("app/Interfaces/", $interface);
-        $url_folder = lcfirst(str_replace("\\", "/", $list['namespace']));
-        Store::folder($url_folder);
 
-        $this->create($url_folder, $list['class']);
+        $this->classFactory->classFactory('app/Interfaces/', $interface);
+        $folder = $this->classFactory->getFolder();
+        $class = $this->classFactory->getClass();
+        $namespace = $this->classFactory->getNamespace();
 
-       	$this->add(
-        	str->of("<?php")->ln()->ln()
-        		->concat('declare(strict_types=1);')->ln()->ln()
-        		->concat('namespace')->spaces(1)
-        		->concat("{$list['namespace']};")->ln()->ln()
-        		->concat('interface')->spaces(1)
-        		->concat($list['class'])->ln()
-        		->concat('{')->ln()->ln()
-        		->concat("}")->ln()
-        		->get()
-        );
+        $this->store->folder($folder);
 
-        $this->force();
-        $this->close();
+        $this->classFactory
+            ->create($class, 'php', $folder)
+            ->add(
+            	Str::of("<?php")->ln()->ln()
+            		->concat('declare(strict_types=1);')->ln()->ln()
+            		->concat('namespace')->spaces(1)
+            		->concat("{$namespace};")->ln()->ln()
+            		->concat('interface')->spaces(1)
+            		->concat($class)->ln()
+            		->concat('{')->ln()->ln()
+            		->concat("}")->ln()
+            		->get()
+            )
+            ->close();
 
         $output->writeln($this->warningOutput("\t>>  INTERFACE: {$interface}"));
 
         $output->writeln(
-        	$this->successOutput("\t>>  INTERFACE: the '{$list['namespace']}\\{$list['class']}' interface has been generated")
+        	$this->successOutput("\t>>  INTERFACE: the '{$namespace}\\{$class}' interface has been generated")
         );
 
 		return Command::SUCCESS;
