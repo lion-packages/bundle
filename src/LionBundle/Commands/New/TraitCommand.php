b9a28dcf10305904e37a4e6d10b8226836665027
Sleon4 <sergioleon4004@hotmail.com>
2023-11-15 18:43:05 -0500

    feat: TraitCommand command added

--- src/LionBundle/Commands/New/TraitCommand.php
+++ src/LionBundle/Commands/New/TraitCommand.php
@@ -1,67 +1,64 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LionBundle\Commands\New;
 
-use App\Traits\Framework\ClassPath;
-use App\Traits\Framework\ConsoleOutput;
+use LionBundle\Helpers\Commands\ClassFactory;
+use LionCommand\Command;
 use LionFiles\Store;
-use Symfony\Component\Console\Command\Command;
+use LionHelpers\Str;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class TraitCommand extends Command
 {
-    use ClassPath, ConsoleOutput;
+    private ClassFactory $classFactory;
+    private Store $store;
 
-	protected static $defaultName = "new:trait";
-
-	protected function initialize(InputInterface $input, OutputInterface $output)
+	protected function initialize(InputInterface $input, OutputInterface $output): void
     {
-
+        $this->classFactory = new ClassFactory();
+        $this->store = new Store();
 	}
 
-	protected function interact(InputInterface $input, OutputInterface $output)
-    {
-
-	}
-
-	protected function configure()
+	protected function configure(): void
     {
         $this
-            ->setDescription("Command required for trait creation")
-            ->addArgument('trait', InputArgument::OPTIONAL, 'Trait name', "ExampleTrait");
+            ->setName('new:trait')
+            ->setDescription('Command required for trait creation')
+            ->addArgument('trait', InputArgument::OPTIONAL, 'Trait name', 'ExampleTrait');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $trait = $input->getArgument('trait');
-        $list = $this->export("app/Traits/", $trait);
-        $url_folder = lcfirst(str_replace("\\", "/", $list['namespace']));
-        Store::folder($url_folder);
 
-        $this->create($url_folder, $list['class']);
+        $this->classFactory->classFactory('app/Traits/', $trait);
+        $folder = $this->classFactory->getFolder();
+        $class = $this->classFactory->getClass();
+        $namespace = $this->classFactory->getNamespace();
 
-        $this->add(
-            str->of('<?php')->ln()->ln()
-                ->concat('declare(strict_types=1);')->ln()->ln()
-                ->concat('namespace')->spaces(1)
-                ->concat("{$list['namespace']};")->ln()->ln()
-                ->concat('trait')->spaces(1)
-                ->concat($list['class'])->spaces(1)->ln()
-                ->concat('{')->ln()->ln()
-                ->concat('}')->ln()
-                ->get()
-        );
+        $this->store->folder($folder);
 
-        $this->force();
-        $this->close();
+        $this->classFactory
+            ->create($class, 'php', $folder)
+            ->add(
+                Str::of('<?php')->ln()->ln()
+                    ->concat('declare(strict_types=1);')->ln()->ln()
+                    ->concat('namespace')->spaces(1)
+                    ->concat("{$namespace};")->ln()->ln()
+                    ->concat('trait')->spaces(1)
+                    ->concat($class)->ln()
+                    ->concat('{')->ln()->ln()
+                    ->concat('}')->ln()
+                    ->get()
+            )
+            ->close();
 
-        $output->writeln($this->warningOutput("\t>>  TRAIT: {$list['class']}"));
-
-        $output->writeln(
-            $this->successOutput("\t>>  TRAIT: the '{$list['namespace']}\\{$list['class']}' trait has been generated")
-        );
+        $output->writeln($this->warningOutput("\t>>  TRAIT: {$class}"));
+        $output->writeln($this->successOutput("\t>>  TRAIT: the '{$namespace}\\{$class}' trait has been generated"));
 
         return Command::SUCCESS;
     }
