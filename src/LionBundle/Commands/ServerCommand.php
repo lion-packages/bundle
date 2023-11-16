2dd6b3ae4ac8186d8787edc7a568c374c018b32d
Sleon4 <sergioleon4004@hotmail.com>
2023-11-15 19:22:52 -0500

    feat: ServerCommand command added

--- src/LionBundle/Commands/ServerCommand.php
+++ src/LionBundle/Commands/ServerCommand.php
@@ -1,43 +1,38 @@
 <?php
 
-namespace LionBundle\Commands\Index;
+declare(strict_types=1);
 
-use App\Traits\Framework\ConsoleOutput;
-use Symfony\Component\Console\Command\Command;
+namespace LionBundle\Commands;
+
+use LionCommand\Command;
+use LionCommand\Kernel;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ServerCommand extends Command
 {
-    use ConsoleOutput;
-
-	protected static $defaultName = "serve";
-
-	protected function initialize(InputInterface $input, OutputInterface $output)
+	protected function initialize(InputInterface $input, OutputInterface $output): void
     {
         $output->write("\033[2J\033[;H");
         $output->write($this->successOutput("\nLion-Framework "));
         $output->writeln("ready in " . number_format((microtime(true) - LION_START), 3) . " ms\n");
 	}
 
-	protected function interact(InputInterface $input, OutputInterface $output)
-    {
-
-	}
-
-	protected function configure()
+	protected function configure(): void
     {
 		$this
-            ->setDescription("Created command to start server locally")
-            ->addOption('port', "p", InputOption::VALUE_OPTIONAL, 'Do you want to set your own port?', 8000)
-            ->addOption('host', "s", InputOption::VALUE_OPTIONAL, 'Do you want to set your own host?', "127.0.0.1");
+            ->setName('serve')
+            ->setDescription('Created command to start server locally')
+            ->addOption('port', 'p', InputOption::VALUE_OPTIONAL, 'Do you want to set your own port?', 8000)
+            ->addOption('host', 's', InputOption::VALUE_OPTIONAL, 'Do you want to set your own host?', '127.0.0.1');
 	}
 
-	protected function execute(InputInterface $input, OutputInterface $output)
+	protected function execute(InputInterface $input, OutputInterface $output): int
     {
 		$port = $input->getOption('port');
         $host = $input->getOption('host');
+
         $url = "{$host}:{$port}";
         $link = "<href=http://{$url}>[http://{$url}]</>";
 
@@ -45,7 +40,8 @@ class ServerCommand extends Command
         $output->writeln($this->warningOutput("\t>>  HOST:</comment> use --host to expose"));
         $output->writeln($this->warningOutput("\t>>  PORT:</comment> use --port to expose"));
         $output->writeln($this->warningOutput("\nPress Ctrl+C to stop the server\n"));
-        kernel->execute("php -S {$host}:{$port} -t public", false);
+
+        (new Kernel())->execute("php -S {$host}:{$port} -t public", false);
 
         return Command::SUCCESS;
 	}
