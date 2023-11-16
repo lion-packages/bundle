52277ac3aedfeeff8bc25caf59878fd7f161ba26
Sleon4 <sergioleon4004@hotmail.com>
2023-11-16 11:32:05 -0500

    feat: NewRSACommand command added

--- src/LionBundle/Commands/RSA/NewRSACommand.php
+++ src/LionBundle/Commands/RSA/NewRSACommand.php
@@ -1,52 +1,51 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LionBundle\Commands\RSA;
 
-use App\Traits\Framework\ConsoleOutput;
+use LionCommand\Command;
 use LionFiles\Store;
 use LionSecurity\RSA;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class NewRSACommand extends Command
 {
-    use ConsoleOutput;
+    private RSA $rsa;
+    private Store $store;
 
-	protected static $defaultName = "rsa:new";
-
-	protected function initialize(InputInterface $input, OutputInterface $output)
+	protected function initialize(InputInterface $input, OutputInterface $output): void
 	{
-
+        $this->rsa = new RSA();
+        $this->store = new Store();
 	}
 
-	protected function interact(InputInterface $input, OutputInterface $output)
-	{
-
-	}
-
-	protected function configure()
+	protected function configure(): void
 	{
 		$this
-            ->setDescription("Command to create public and private keys with RSA")
+            ->setName('rsa:new')
+            ->setDescription('Command to create public and private keys with RSA')
             ->addOption('path', 'p', InputOption::VALUE_REQUIRED, 'Save to a specific path?');
 	}
 
-	protected function execute(InputInterface $input, OutputInterface $output)
+	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
         $path = $input->getOption('path');
-        RSA::setPath($path === null ? RSA::getPath() : storage_path($path, false));
-		Store::folder(RSA::getPath());
-		RSA::createKeys();
 
-        if (isSuccess(Store::exist(".rnd"))) {
-            Store::remove('.rnd');
+        $this->rsa->setUrlPath($path === null ? $this->rsa->getUrlPath() : storage_path($path, false));
+        $this->store->folder($this->rsa->getUrlPath());
+        $this->rsa->create();
+
+        if (isSuccess($this->store->exist('.rnd'))) {
+            $this->store->remove('.rnd');
         }
 
         $output->writeln($this->warningOutput("\t>>  RSA KEYS: public and private"));
-        $output->writeln($this->successOutput("\t>>  RSA KEYS: Exported in " . RSA::getPath() . "public.key"));
-        $output->writeln($this->successOutput("\t>>  RSA KEYS: Exported in " . RSA::getPath() . "private.key"));
+        $output->writeln($this->successOutput("\t>>  RSA KEYS: Exported in {$this->rsa->getUrlPath()}public.key"));
+        $output->writeln($this->successOutput("\t>>  RSA KEYS: Exported in {$this->rsa->getUrlPath()}private.key"));
+
 		return Command::SUCCESS;
 	}
 }
