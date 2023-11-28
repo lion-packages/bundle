79d176da1b1cbb8b4804380f44e5fbefbac4fd1b
Sleon4 <sergioleon4004@hotmail.com>
2023-11-27 21:22:37 -0500

    feat: factory has been added to generate files from commands

---
+++ src/LionBundle/Helpers/Commands/ClassCommandFactory.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LionBundle\Helpers\Commands;
+
+use Closure;
+use LionBundle\Helpers\Commands\ClassFactory;
+use LionFiles\Store;
+
+class ClassCommandFactory
+{
+    private ?array $factories = null;
+
+    public function __construct(array $classFactoryCommands)
+    {
+        $this->addClassFactory($classFactoryCommands);
+    }
+
+    private function addClassFactory(array $classFactoryCommands): void
+    {
+        foreach ($classFactoryCommands as $classFactory) {
+            $this->factories[$classFactory] = new ClassFactory();
+        }
+    }
+
+    public function execute(Closure $process): int
+    {
+        return $process($this, new Store());
+    }
+
+    public function getFactories(): ?array
+    {
+        return $this->factories;
+    }
+
+    public function setFactories(?array $factories): void
+    {
+        $this->factories = $factories;
+    }
+
+    public function getFactory(string $factory): ClassFactory
+    {
+        return $this->factories[$factory];
+    }
+
+    public function getData(ClassFactory $classFactory, array $data): object
+    {
+        $classFactory->classFactory($data['path'], $data['class']);
+
+        return (object) [
+            'folder' => $classFactory->getFolder(),
+            'class' => $classFactory->getClass(),
+            'namespace' => $classFactory->getNamespace()
+        ];
+    }
+}
