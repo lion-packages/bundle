118e1d21a2a7f11ba0c8a9f29e66593cfef5eeb9
Sleon4 <sergioleon4004@hotmail.com>
2024-01-05 11:24:10 -0500

    feat: NewSeedCommand command added

---
+++ src/LionBundle/Commands/DB/Seed/NewSeedCommand.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LionBundle\Commands\DB\Seed;
+
+use LionBundle\Helpers\Commands\ClassFactory;
+use LionCommand\Command;
+use LionFiles\Store;
+use LionHelpers\Str;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NewSeedCommand extends Command
+{
+    private ClassFactory $classFactory;
+    private Store $store;
+    private Str $str;
+
+    /**
+     * @required
+     * */
+    public function setClassFactory(ClassFactory $classFactory): NewSeedCommand
+    {
+        $this->classFactory = $classFactory;
+
+        return $this;
+    }
+
+    /**
+     * @required
+     * */
+    public function setStore(Store $store): NewSeedCommand
+    {
+        $this->store = $store;
+
+        return $this;
+    }
+
+    /**
+     * @required
+     * */
+    public function setStr(Str $str): NewSeedCommand
+    {
+        $this->str = $str;
+
+        return $this;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName('db:seed:new')
+            ->setDescription('Command required for creating new seeds')
+            ->addArgument('seed', InputArgument::OPTIONAL, 'Name seed', 'ExampleSeed');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->classFactory->classFactory('database/Seed/', $input->getArgument('seed'));
+        $folder = $this->classFactory->getFolder();
+        $class = $this->classFactory->getClass();
+        $namespace = $this->classFactory->getNamespace();
+
+        $this->store->folder($folder);
+
+        $this->classFactory
+            ->create($class, 'php', $folder)
+            ->add($this->str->of("<?php")->ln()->ln()->concat('declare(strict_types=1);')->ln()->ln()->get())
+            ->add($this->str->of("namespace ")->concat($namespace)->concat(";")->ln()->ln()->get())
+            ->add($this->str->of("use LionDatabase\Drivers\MySQL as DB;")->ln()->ln()->get())
+            ->add($this->str->of("class ")->concat($class)->ln()->concat("{")->ln()->get())
+            ->add("\t/**\n")
+            ->add("\t * Seed the application's database\n")
+            ->add("\t **/\n")
+            ->add("\tpublic function run(): object\n\t{\n\t\treturn DB::call('stored_procedure', [])->execute();\n\t}\n}\n")
+            ->close();
+
+        $output->writeln($this->warningOutput("\t>>  SEED: {$class}"));
+        $output->writeln($this->successOutput("\t>>  SEED: the '{$namespace}\\{$class}' seed has been generated"));
+
+        return Command::SUCCESS;
+    }
+}
