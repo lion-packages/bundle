a2e25e763e82e49df83e140feb095955530a5d0c
Sleon4 <sergioleon4004@hotmail.com>
2024-01-23 18:17:13 -0500

    feat: NpmRunBuildCommand command has been added

--- src/LionBundle/Commands/Npm/NpmRunBuildCommand.php
+++ src/LionBundle/Commands/Npm/NpmRunBuildCommand.php
@@ -1,41 +1,85 @@
 <?php
 
-namespace LionBundle\Commands\Npm;
+declare(strict_types=1);
 
-use App\Traits\Framework\ConsoleOutput;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+namespace Lion\Bundle\Commands\Npm;
+
+use Lion\Bundle\Helpers\RedisClient;
+use Lion\Command\Command;
+use Lion\Command\Kernel;
+use Lion\Helpers\Arr;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class NpmRunBuildCommand extends Command
 {
-	use ConsoleOutput;
+    private RedisClient $redisClient;
+    private Arr $arr;
+    private Kernel $kernel;
 
-	protected static $defaultName = "npm:build";
+    /**
+     * @required
+     * */
+    public function setRedisClient(RedisClient $redisClient): NpmRunBuildCommand
+    {
+        $this->redisClient = $redisClient;
 
-	protected function initialize(InputInterface $input, OutputInterface $output)
-	{
+        return $this;
+    }
 
-	}
+    /**
+     * @required
+     * */
+    public function setArr(Arr $arr): NpmRunBuildCommand
+    {
+        $this->arr = $arr;
 
-	protected function interact(InputInterface $input, OutputInterface $output)
-	{
+        return $this;
+    }
 
-	}
+    /**
+     * @required
+     * */
+    public function setKernel(Kernel $kernel): NpmRunBuildCommand
+    {
+        $this->kernel = $kernel;
 
-	protected function configure()
+        return $this;
+    }
+
+	protected function configure(): void
 	{
 		$this
-            ->setDescription("Command to generate dist for a vite project")
-            ->addArgument("project", InputArgument::REQUIRED, "Project name");
+            ->setName('npm:build')
+            ->setDescription('Command to generate dist for a vite project');
 	}
 
-	protected function execute(InputInterface $input, OutputInterface $output)
+	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-		$project = $input->getArgument("project");
-        $cmd = kernel->execute("cd vite/{$project}/ && npm run build", false);
-        $output->writeln(arr->of($cmd)->join("\n"));
+        $projects = $this->arr->of($this->redisClient->getClient()->hgetall('vite'))->keys()->get();
+        $project = $this->selectedProject($input, $output, $projects);
+
+        $cmdOutput = $this->kernel->execute("cd vite/{$project}/ && npm run build", false);
+        $output->writeln($this->arr->of($cmdOutput)->join("\n"));
+
 		return Command::SUCCESS;
 	}
+
+    private function selectedProject(InputInterface $input, OutputInterface $output, array $projects): string
+    {
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
+
+        return $helper->ask(
+            $input,
+            $output,
+            new ChoiceQuestion(
+                ('Select project ' . $this->warningOutput('(default: ' . reset($projects) . ')')),
+                $projects,
+                0
+            )
+        );
+    }
 }
