16ae2512745bb171d821f47c88e8ea122b28bd44
Sleon4 <sergioleon4004@hotmail.com>
2024-01-25 16:16:48 -0500

    feat: NpmInitCommand command added

--- src/LionBundle/Commands/Npm/NpmInitCommand.php
+++ src/LionBundle/Commands/Npm/NpmInitCommand.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Lion\Bundle\Commands\Npm;
 
-use Lion\Bundle\Commands\Lion\Npm\NpmInitCommand as NpmNpmInitCommand;
+use Lion\Bundle\Commands\Lion\Npm\NpmInitCommand as LionNpmInitCommand;
 
-class NpmInitCommand extends NpmNpmInitCommand
+class NpmInitCommand extends LionNpmInitCommand
 {
     protected function configure(): void
     {
