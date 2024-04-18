261e8a565241306f304bceb914c440be3440beda
Sleon4 <sergioleon4004@hotmail.com>
2024-04-18 10:46:40 -0500

    feat: HtmlCommand has been added

---
+++ src/LionBundle/Commands/Lion/New/HtmlCommand.php
@@ -0,0 +1,157 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lion\Bundle\Commands\Lion\New;
+
+use Lion\Bundle\Helpers\Commands\ClassFactory;
+use Lion\Command\Command;
+use Lion\Files\Store;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Generate classes to generate templates in HTML
+ *
+ * @property ClassFactory $classFactory [Fabricates the data provided to
+ * manipulate information (folder, class, namespace)]
+ * @property Store $store [Store class object]
+ *
+ * @package Lion\Bundle\Commands\Lion\New
+ */
+class HtmlCommand extends Command
+{
+    /**
+     * [Fabricates the data provided to manipulate information (folder, class,
+     * namespace)]
+     *
+     * @var ClassFactory $classFactory
+     */
+    private ClassFactory $classFactory;
+
+    /**
+     * [Store class object]
+     *
+     * @var Store $store
+     */
+    private Store $store;
+
+    /**
+     * @required
+     */
+    public function setClassFactory(ClassFactory $classFactory): void
+    {
+        $this->classFactory = $classFactory;
+    }
+
+    /**
+     * @required
+     */
+    public function setStore(Store $store): void
+    {
+        $this->store = $store;
+    }
+
+    /**
+     * Configures the current command
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setName('new:html')
+            ->setDescription('Command needed to create new HTML templates')
+            ->addArgument('html', InputArgument::OPTIONAL, 'Html name', 'ExampleHtml');
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * This method is not abstract because you can use this class
+     * as a concrete class. In this case, instead of defining the
+     * execute() method, you set the code to execute by passing
+     * a Closure to the setCode() method.
+     *
+     * @param InputInterface $input [InputInterface is the interface implemented
+     * by all input classes]
+     * @param OutputInterface $output [OutputInterface is the interface
+     * implemented by all Output classes]
+     *
+     * @return int 0 if everything went fine, or an exit code
+     *
+     * @throws LogicException When this abstract method is not implemented
+     *
+     * @see setCode()
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $html = $input->getArgument('html');
+
+        $this->classFactory->classFactory('app/Html/', $html);
+
+        $folder = $this->classFactory->getFolder();
+
+        $namespace = $this->classFactory->getNamespace();
+
+        $class = $this->classFactory->getClass();
+
+        $this->store->folder($folder);
+
+        $this->classFactory
+            ->create($class, ClassFactory::PHP_EXTENSION, $folder)
+            ->add(
+                <<<PHP
+                <?php
+
+                declare(strict_types=1);
+
+                namespace {$namespace};
+
+                use Lion\Bundle\Helpers\Commands\Html;
+                use Lion\Bundle\Interface\HtmlInterface;
+
+                /**
+                 * Define an HTML template
+                 *
+                 * @package {$namespace}
+                 */
+                class {$class} extends Html implements HtmlInterface
+                {
+                    /**
+                     * {@inheritdoc}
+                     */
+                    public function template(): {$class}
+                    {
+                        \$this->add(
+                            <<<HTML
+                            <!DOCTYPE html>
+                            <html>
+                            <head>
+                                <meta charset="utf-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1">
+                                <title>HTML Template</title>
+                            </head>
+                            <body>
+                                <h1>HTML Template (--REPLACE--)</h1>
+                            </body>
+                            </html>
+                            HTML
+                        );
+
+                        return \$this;
+                    }
+                }
+
+                PHP
+            )
+            ->close();
+
+        $output->writeln($this->warningOutput("\t>>  HTML: {$class}"));
+
+        $output->writeln($this->successOutput("\t>>  HTML: the '{$namespace}\\{$class}' html has been generated"));
+
+        return Command::SUCCESS;
+    }
+}
--- tests/Commands/Lion/New/HtmlCommandTest.php
+++ tests/Commands/Lion/New/HtmlCommandTest.php
@@ -4,22 +4,23 @@ declare(strict_types=1);
 
 namespace Tests\Commands\Lion\New;
 
-use Lion\Bundle\Commands\Lion\New\FactoryCommand;
+use Lion\Bundle\Commands\Lion\New\HtmlCommand;
+use Lion\Bundle\Helpers\Commands\Html;
+use Lion\Bundle\Interface\HtmlInterface;
 use Lion\Command\Command;
 use Lion\Command\Kernel;
 use Lion\Dependency\Injection\Container;
 use Lion\Test\Test;
 use Symfony\Component\Console\Tester\CommandTester;
 
-class FactoryCommandTest extends Test
+class HtmlCommandTest extends Test
 {
-    const URL_PATH = './database/Factory/';
-    const NAMESPACE_CLASS = 'Database\\Factory\\';
-    const CLASS_NAME = 'TestFactory';
+    const URL_PATH = './app/Html/';
+    const NAMESPACE_CLASS = 'App\\Html\\';
+    const CLASS_NAME = 'TestHtml';
     const OBJECT_NAME = self::NAMESPACE_CLASS . self::CLASS_NAME;
     const FILE_NAME = self::CLASS_NAME . '.php';
-    const OUTPUT_MESSAGE = 'factory has been generated';
-    const METHOD = ['columns', 'definition'];
+    const OUTPUT_MESSAGE = 'html has been generated';
 
     private CommandTester $commandTester;
 
@@ -27,9 +28,9 @@ class FactoryCommandTest extends Test
     {
         $application = (new Kernel())->getApplication();
 
-        $application->add((new Container())->injectDependencies(new FactoryCommand()));
+        $application->add((new Container())->injectDependencies(new HtmlCommand()));
 
-        $this->commandTester = new CommandTester($application->find('new:factory'));
+        $this->commandTester = new CommandTester($application->find('new:html'));
 
         $this->createDirectory(self::URL_PATH);
     }
@@ -41,19 +42,18 @@ class FactoryCommandTest extends Test
 
     public function testExecute(): void
     {
-        $this->assertSame(Command::SUCCESS, $this->commandTester->execute(['factory' => self::CLASS_NAME]));
+        $this->assertSame(Command::SUCCESS, $this->commandTester->execute(['html' => self::CLASS_NAME]));
         $this->assertStringContainsString(self::OUTPUT_MESSAGE, $this->commandTester->getDisplay());
         $this->assertFileExists(self::URL_PATH . self::FILE_NAME);
 
         $objClass = new (self::OBJECT_NAME)();
 
         $this->assertIsObject($objClass);
-        $this->assertInstanceOf(self::OBJECT_NAME, $objClass);
-        $this->assertSame(self::METHOD, get_class_methods($objClass));
 
-        $definition = $objClass->definition();
-
-        $this->assertIsArray($definition);
-        $this->assertCount(1, $definition);
+        $this->assertInstances($objClass, [
+            self::OBJECT_NAME,
+            Html::class,
+            HtmlInterface::class
+        ]);
     }
 }
