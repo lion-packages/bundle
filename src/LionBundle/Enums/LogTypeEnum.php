31caa90d37a4c51d03fdc75e4927fd4b3ec84841
Sleon4 <sergioleon4004@hotmail.com>
2024-05-16 09:27:09 -0500

    test: added test for LogTypeEnum

--- src/LionBundle/Enums/LogTypeEnum.php
+++ src/LionBundle/Enums/LogTypeEnum.php
@@ -12,42 +12,42 @@ namespace Lion\Bundle\Enums;
 enum LogTypeEnum: string
 {
     /**
-     * Adds a log record at the DEBUG level
+     * [Adds a log record at the DEBUG level]
      */
     case DEBUG = 'debug';
 
     /**
-     * Adds a log record at the INFO level
+     * [Adds a log record at the INFO level]
      */
     case INFO = 'info';
 
     /**
-     * Adds a log record at the NOTICE level
+     * [Adds a log record at the NOTICE level]
      */
     case NOTICE = 'notice';
 
     /**
-     * Adds a log record at the WARNING level
+     * [Adds a log record at the WARNING level]
      */
     case WARNING = 'warning';
 
     /**
-     * Adds a log record at the ERROR level
+     * [Adds a log record at the ERROR level]
      */
     case ERROR = 'error';
 
     /**
-     * Adds a log record at the CRITICAL level
+     * [Adds a log record at the CRITICAL level]
      */
     case CRITICAL = 'critical';
 
     /**
-     * Adds a log record at the ALERT level
+     * [Adds a log record at the ALERT level]
      */
     case ALERT = 'alert';
 
     /**
-     * Adds a log record at the EMERGENCY level
+     * [Adds a log record at the EMERGENCY level]
      */
     case EMERGENCY = 'emergency';
 }
