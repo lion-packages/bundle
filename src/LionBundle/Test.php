3a02958503224a5185fca73b01a8730ce60196f4
Sleon4 <sergioleon4004@hotmail.com>
2024-11-24 11:19:42 -0500

    feat: executeSeedsGroup is added

--- src/LionBundle/Test.php
+++ src/LionBundle/Test.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Lion\Bundle;
 
 use Lion\Bundle\Helpers\Commands\Migrations\Migrations;
+use Lion\Bundle\Helpers\Commands\Seeds\Seeds;
 use Lion\Test\Test as Testing;
 
 /**
@@ -12,6 +13,8 @@ use Lion\Test\Test as Testing;
  *
  * @property Migrations|null $migrations [Manages the processes of creating or
  * executing migrations]
+ * @property Seeds|null $seeds [Manages the processes of creating or executing
+ * seeds]
  *
  * @package Lion\Bundle
  */
@@ -25,6 +28,13 @@ class Test extends Testing
     private ?Migrations $migrations = null;
 
     /**
+     * [Manages the processes of creating or executing seeds]
+     *
+     * @var Seeds|null $seeds
+     */
+    private ?Seeds $seeds = null;
+
+    /**
      * Run a group of migrations
      *
      * @param array<int, string> $migrations [List of classes]
@@ -33,10 +43,26 @@ class Test extends Testing
      */
     protected function executeMigrationsGroup(array $migrations): void
     {
-        if (null === $this->migrations) {
+        if (NULL_VALUE === $this->migrations) {
             $this->migrations = new Migrations();
         }
 
         $this->migrations->executeMigrationsGroup($migrations);
+    }
+
+    /**
+     * Run a group of seeds
+     *
+     * @param array $seeds [List of classes]
+     *
+     * @return void
+     */
+    protected function executeSeedsGroup(array $seeds): void
+    {
+        if (NULL_VALUE === $this->seeds) {
+            $this->seeds = new Seeds();
+        }
+
+        $this->seeds->executeSeedsGroup($seeds);
     }
 }
---
+++ tests/Helpers/Commands/Seeds/SeedsTest.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Helpers\Commands\Seeds;
+
+use Lion\Bundle\Commands\Lion\New\SeedCommand;
+use Lion\Bundle\Helpers\Commands\ClassFactory;
+use Lion\Bundle\Helpers\Commands\Migrations\Migrations;
+use Lion\Bundle\Helpers\Commands\Seeds\Seeds;
+use Lion\Bundle\Interface\SeedInterface;
+use Lion\Command\Command;
+use Lion\Files\Store;
+use Lion\Test\Test;
+use PHPUnit\Framework\Attributes\Test as Testing;
+use ReflectionException;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class SeedsTest extends Test
+{
+    private const string SEED_NAME = 'TestSeed';
+    private const string FILE_NAME = self::SEED_NAME . '.php';
+    private const string URL_PATH_SEED = 'database/Seed/';
+    private const string NAMESPACE_SEED = 'Database\\Seed\\';
+    private const string MESSAGE_OUTPUT = 'seed has been generated';
+
+    private CommandTester $commandTester;
+    private Seeds $seeds;
+
+    /**
+     * @throws ReflectionException
+     */
+    protected function setUp(): void
+    {
+        $this->seeds = (new Seeds())
+            ->setMigrations(new Migrations());
+
+        $application = new Application();
+
+        $application->add(
+            (new SeedCommand())
+                ->setClassFactory(
+                    (new ClassFactory())
+                        ->setStore(new Store())
+                )
+                ->setStore(new Store())
+        );
+
+        $this->commandTester = new CommandTester($application->find('new:seed'));
+
+        $this->initReflection($this->seeds);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->rmdirRecursively('database/');
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    #[Testing]
+    public function setMigrations(): void
+    {
+        $this->assertInstanceOf(Seeds::class, $this->seeds->setMigrations(new Migrations()));
+        $this->assertInstanceOf(Migrations::class, $this->getPrivateProperty('migrations'));
+    }
+
+    #[Testing]
+    public function executeSeedsGroup(): void
+    {
+        $this->assertSame(Command::SUCCESS, $this->commandTester->execute(['seed' => self::SEED_NAME]));
+        $this->assertStringContainsString(self::MESSAGE_OUTPUT, $this->commandTester->getDisplay());
+        $this->assertFileExists(self::URL_PATH_SEED . self::FILE_NAME);
+
+        $classObject = new (self::NAMESPACE_SEED . self::SEED_NAME)();
+
+        $this->assertIsObject($classObject);
+
+        $this->assertInstances($classObject, [
+            SeedInterface::class,
+            self::NAMESPACE_SEED . self::SEED_NAME,
+        ]);
+
+        ob_start();
+
+        $this->seeds->executeSeedsGroup([
+            self::NAMESPACE_SEED . self::SEED_NAME,
+        ]);
+
+        $output = ob_get_clean();
+
+        $this->assertStringContainsString('Database\Seed\TestSeed', $output);
+        $this->assertStringContainsString('run seed', $output);
+        $this->assertStringContainsString('Seed group executed successfully', $output);
+    }
+}
